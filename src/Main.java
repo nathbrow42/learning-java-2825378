4a9b30d719fa65986c184723bd99fe863367250f
nathbrow <37987956+nathbrow42@users.noreply.github.com>
2021-08-07 18:51:07 -0400

    Completed Chap 2
    Created project in IntelliJ and worked through chapter 2 exercises

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double studentGPA = 2.75;
+        String studentFirstName = "Nathalie";
+        String studentLastName = "Brown";
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
+
+    }
+}
