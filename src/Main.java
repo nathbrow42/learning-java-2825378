2a445c3e0fb3bcd1f9dce67ef91a24532406d230
nathbrow <37987956+nathbrow42@users.noreply.github.com>
2021-08-07 22:04:13 -0400

    Finished chap 3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        String question = "What is my cat's name?";
+        String choiceOne = "Oscar";
+        String choiceTwo = "Boca";
+        String choiceThree = "Doc McStuffins";
+        String correctAnswer = choiceTwo;
 
-        double studentGPA = 2.75;
-        String studentFirstName = "Nathalie";
-        String studentLastName = "Brown";
+        //Write a print statement asking the question
+        //Write a print statement giving the answer choices
 
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
-        System.out.println("What do you want to update it to?");
+        System.out.println(question);
+        System.out.println("The choices are: " + choiceOne + ", " + choiceTwo + ", or " + choiceThree);
+
+        //Have the user input an answer
+        //Retrieve the user's input
 
         Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
+        String userInput = input.next();
 
-        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
+        //If the user's input matches the correctAnswer...
+        //then the user is correct and we want to print out a congrats message to the user.
 
+        if(userInput.equals(correctAnswer)) {
+            System.out.println("You are correct!");
+        }
+
+        //If the user's input does not match the correctAnswer...
+        //then the user is incorrect and we want to print out a message saying that the user is wrong
+
+        else {
+            System.out.println("Sorry, that is the wrong answer. His name is Boca");
+        }
     }
 }
+
